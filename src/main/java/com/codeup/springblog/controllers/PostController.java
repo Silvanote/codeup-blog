3eebff72fb6e5fdbcff20b81f9be7bf1f79f35a4
Juan Silva <juan.silvawd@gmail.com>
2022-08-02 22:48:53 -0500

    post

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,5 +1,30 @@
 package com.codeup.springblog.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class PostController {
-    
+    @RequestMapping(path = "/posts", method = RequestMethod.GET)
+    public String viewAllPosts() {
+        return "/posts/index";
+    }
+
+    @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
+    public String viewIndividualPosts(@PathVariable long id) {
+        return "/posts/view";
+    }
+
+    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
+    public String viewCreatePostForm() {
+        return "/posts/create";
+    }
+
+    @PostMapping("/posts/create")
+    public String savePost() {
+        return null;
+    }
 }
