76bb93ce36b6490072ca296eb4e842d4b09e1210
Juan Silva <juan.silvawd@gmail.com>
2022-08-05 14:02:31 -0500

    yesterday ex

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -4,10 +4,7 @@ import com.codeup.springblog.models.Post;
 import com.codeup.springblog.repositories.PostRespository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,14 +14,14 @@ public class PostController {
 
     private PostRespository postsDao;
 
-    public PostController(PostRespository postsDao){
+    public PostController(PostRespository postsDao) {
         this.postsDao = postsDao;
     }
 
 
     @RequestMapping(path = "/posts", method = RequestMethod.GET)
-    public String viewAllPosts(Model model){
-    List<Post> allPosts = new ArrayList<>();
+    public String viewAllPosts(Model model) {
+        List<Post> allPosts = new ArrayList<>();
         allPosts.add(new Post(1, "Number 1", "Using Spring"));
         allPosts.add(new Post(2, "Number 2", "Using Spring"));
         allPosts.add(new Post(3, "Number 3", "Using Spring"));
@@ -32,12 +29,23 @@ public class PostController {
 
         model.addAttribute("posts", allPosts);
 
+        private PostRespository postsDoa;
+
+        public PostController(PostRespository postsDao)
+        this.postsDao = postsDoa;
+    }
+
+    @RequestMapping(path = "/posts", method = RequestMethod.GET)
+    public String viewAllPosts(Model model) {
+        model.addAttribute("posts", postsDao.findAll());
         return "/posts/index";
+
     }
 
     @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
     public String viewIndividualPosts(@PathVariable long id, Model model) {
         model.addAttribute("Trucks", new Post(5, "Ford Trucks", "New details"));
+        model.addAttribute("Trucks", postsDao.getById(id));
         return "/posts/show";
     }
 
@@ -49,5 +57,10 @@ public class PostController {
     @PostMapping("/posts/create")
     public String savePost() {
         return null;
+    }
+
+    public String savePost(@RequestParam(name = "title") String title, @RequestParam(name = "body") String body) {
+        postsDao.save(new Post(title, body));
+        return "redirect:/posts";
     }
 }
